48ecb95dc0e0e944982e17c160722d06071f22a7
anik-aryan <127028550+anik-aryan@users.noreply.github.com>
2025-06-16 15:30:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,17 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('static-v1').then(cache => {
-  return cache.addAll([
-    './',
-    './index.html',
-    './styles.css',
-    './script.js',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
-  ]);
-});
-
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('static-v1')
+    .then(cache => cache.addAll([
+      './',
+      './index.html',
+      './styles.css',
+      './script.js',
+      './manifest.json',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
